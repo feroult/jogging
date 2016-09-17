f8fda83116860952c423d83f1d0240476a69f908
feroult <feroult@gmail.com>
2016-09-17 11:17:22 -0300

    test assert validation

--- api/src/test/java/jogging/models/user/UserSignUpActionTest.java
+++ api/src/test/java/jogging/models/user/UserSignUpActionTest.java
@@ -2,10 +2,19 @@ package jogging.models.user;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class UserSignUpActionTest extends UserTestCase {
 
     @Test
     public void testUserSignUp() {
+        assertPostWithStatus("/users/sign-up", JOHN_SIGNUP_JSON, 200);
+        
+        assertEquals("john", id(User.class, "john").fetch().name);
+    }
+
+    @Test
+    public void testUsersCannotSignUpTwice() {
         assertPostWithStatus("/users/sign-up", JOHN_SIGNUP_JSON, 200);
         assertPostWithStatus("/users/sign-up", JOHN_SIGNUP_JSON, 409);
     }
